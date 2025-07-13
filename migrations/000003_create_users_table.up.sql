83758b9255b9c8f844fcb1fc538a3823ca469e86
yusufbahtiarr <yusufbdev@gmail.com>
2025-07-14 00:43:23 +0700

    fix(migration): correct foreign key reference to profiles table

--- migrations/000003_create_users_table.up.sql
+++ migrations/000003_create_users_table.up.sql
@@ -3,7 +3,7 @@ CREATE TABLE users (
   email VARCHAR(100) NOT NULL UNIQUE,
   password VARCHAR(100) NOT NULL,
   role role_type DEFAULT 'user',
-  id_profile INT NOT NULL REFERENCES profile(id) ON DELETE CASCADE,
+  id_profile INT NOT NULL REFERENCES profiles(id) ON DELETE CASCADE,
   created_at TIMESTAMP(0) DEFAULT now(),
   updated_at TIMESTAMP(0) DEFAULT now()
 );
