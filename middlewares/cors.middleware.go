55109f4328543720af83326c6dfb4ac5301e024a
yusufbahtiarr <yusufbdev@gmail.com>
2025-07-16 02:16:46 +0700

    refactor: change Allow Origin in CORSMiddleware

--- middlewares/cors.middleware.go
+++ middlewares/cors.middleware.go
@@ -7,8 +7,8 @@ import (
 
 func CORSMiddlewate() gin.HandlerFunc {
 	return cors.New(cors.Config{
-		AllowOrigins:  []string{"http://146.190.102.54:9702", "http://localhost:8888", "http://localhost:5173"},
-		AllowMethods:  []string{"GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"},
+		AllowOrigins:  []string{"http://146.190.102.54:9702", "http://localhost:8888"},
+		AllowMethods:  []string{"GET", "POST", "PUT", "DELETE", "PATCH"},
 		AllowHeaders:  []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders: []string{"Content-Length"},
 	})
