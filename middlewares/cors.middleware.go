f189e82c8c328ce5bbe1fa689a6538df3b0d2aac
yusufbahtiarr <yusufbdev@gmail.com>
2025-07-29 22:56:18 +0700

    chore: change allow origin in CORS config

--- middlewares/cors.middleware.go
+++ middlewares/cors.middleware.go
@@ -7,7 +7,7 @@ import (
 
 func CORSMiddleware() gin.HandlerFunc {
 	return cors.New(cors.Config{
-		AllowOrigins:  []string{"http://146.190.102.54:9702", "http://localhost:8888", "http://localhost:5173", "https://kukky.vercel.app"},
+		AllowOrigins:  []string{"*"},
 		AllowMethods:  []string{"GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"},
 		AllowHeaders:  []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders: []string{"Content-Length"},
